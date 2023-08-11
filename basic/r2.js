52e0b1e91bc7451bebb19859066dade3eb019811
dom <nhatpq@mageplaza.com>
2023-08-11 11:05:58 +0700

    update r2

--- basic/r2.js
+++ basic/r2.js
@@ -1,15 +1,24 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const apiUsers = 'https://jsonplaceholder.typicode.com/users';
 
 async function fetchDataFromAPI(apiUrl) {
     try {
         const response = await axios.get(apiUrl);
-        console.log(JSON.stringify(response.data, null, 2));
+        return response.data;
     } catch (error) {
         console.error('Error: ', error.message);
         return [];
     }
 }
 
-fetchDataFromAPI(apiUsers)
+(async () => {
+    try {
+        const data = await fetchDataFromAPI(apiUsers);
+        fs.writeFileSync('./r2.json', JSON.stringify(data, null, 2));
+        console.log('Data written to r2.json');
+    } catch (error) {
+        console.error('Error: ', error.message);
+    }
+})();
